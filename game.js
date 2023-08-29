542666e6c03b7810ce30a14720cccd7490966c49
SammedJainDP <117413524+SammedJainDP@users.noreply.github.com>
2023-08-29 18:38:21 -0400

    20

--- game.js
+++ game.js
@@ -1,9 +1,10 @@
 let lastRenderTime= 0
-const SNAKE_SPEED=1
+const SNAKE_SPEED=20
 function main(currentTime){
     window.requestAnimationFrame(main)
     const secondsSinceLastRender=(currentTime-lastRenderTime)/1000
-    if(secondsSinceLastRender<1/SNAKE_SPEED)return
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    
     console.log('Render')
     lastRenderTime=currentTime
     
