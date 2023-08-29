6e8c1d6e862af0812544917da826a14670b437c8
SammedJainDP <117413524+SammedJainDP@users.noreply.github.com>
2023-08-29 18:27:39 -0400

    checking if the render value is correct

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
 let lastRenderTime= 0
+const SNAKE_SPEED=1
 function main(currentTime){
     window.requestAnimationFrame(main)
+    const secondsSinceLastRender=(currentTime-lastRenderTime)/1000
+    if(secondsSinceLastRender<1/SNAKE_SPEED)return
+    console.log('Render')
     lastRenderTime=currentTime
-    console.log(currentTime)
+    
 }
 
 window.requestAnimationFrame(main)
